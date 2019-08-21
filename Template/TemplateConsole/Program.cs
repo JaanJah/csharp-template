54e1e544ec67ed4e4eb6a4d44eea14e8a96196e4
JaanJahimees <jaanjahilo@outlook.com>
2019-08-21 21:45:33 +0300

    Add project and Dockerfile

---
+++ Template/TemplateConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
